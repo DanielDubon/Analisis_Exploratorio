8ccecb07e065d2d188be6a1994bd51fac5af7a23
DanielDubon <dub22233@uvg.edu.gt>
2025-01-29 23:39:53 -0600

    feat: sumary data information was added

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ import pandas as pd
 
 movies = pd.read_csv('movies.csv', encoding='ISO-8859-1')
 
+summary_statistics = movies.describe()
 
-print(movies.head())
+data_info = movies.info()
+
+print(summary_statistics)
+
+print(data_info)
