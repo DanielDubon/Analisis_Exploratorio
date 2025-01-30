0f76dc34f569b1e6378e4e6747413e40a6a90d16
DanielDubon <dub22233@uvg.edu.gt>
2025-01-30 00:26:01 -0600

    feat: ex 2 with quantitative and qualitative data analized script was added

--- main.py
+++ main.py
@@ -1,12 +1,71 @@
 import pandas as pd
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 movies = pd.read_csv('movies.csv', encoding='ISO-8859-1')
 
-summary_statistics = movies.describe()
 
-data_info = movies.info()
+#-----------
+#Ejercicio 1 resumen estadistico:
 
-print(summary_statistics)
+#summary_statistics = movies.describe()
+#data_info = movies.info()
+#print(summary_statistics)
+#print(data_info)
 
-print(data_info)
+#-----------
+
+#-----------
+#Ejercicio 2 Investigacion de la distribucion Normal y tablas de frecuencias de las variables cualitativas:
+
+movies['castWomenAmount'] = pd.to_numeric(movies['castWomenAmount'], errors='coerce')
+movies['castMenAmount'] = pd.to_numeric(movies['castMenAmount'], errors='coerce')
+
+quantitative_vars = ['id', 'budget', 'revenue', 'runtime', 'popularity', 'voteAvg', 'voteCount', 
+                     'genresAmount', 'productionCoAmount', 'productionCountriesAmount', 
+                     'actorsAmount', 'castWomenAmount', 'castMenAmount']
+
+# Verificar la normalidad de las variables cuantitativas usando la prueba de Anderson-Darling
+normality_results = {}
+for var in quantitative_vars:
+    ad_stat, critical_values, significance_level = stats.anderson(movies[var].dropna(), dist='norm')
+    normality_results[var] = (ad_stat, critical_values)
+
+print("Resultados de la prueba de normalidad (Estadístico de Anderson-Darling):")
+for var, (ad_stat, critical_values) in normality_results.items():
+    print(f"{var}: Estadístico = {ad_stat:.4f}, Valores críticos = {critical_values}")
+    if ad_stat < critical_values[2]:  # Usando el nivel de significancia del 5%
+        print(f" - {'Normal'}")
+    else:
+        print(f" - {'No Normal'}")
+
+
+qualitative_vars = ['genres', 'homePage', 'productionCompany', 'productionCompanyCountry', 
+                    'productionCountry', 'video', 'director', 'actors', 'originalTitle', 
+                    'title', 'originalLanguage', 'releaseDate']
+
+frequency_tables = {}
+for var in qualitative_vars:
+    frequency_tables[var] = movies[var].value_counts()
+
+print("\nTablas de frecuencias para variables cualitativas:")
+for var, freq in frequency_tables.items():
+    print(f"\nFrecuencias de {var}:")
+    print(freq)
+
+
+for var in quantitative_vars:
+    plt.figure(figsize=(10, 6))
+    sns.histplot(movies[var].dropna(), kde=True, bins=30)
+    plt.title(f'Distribución de {var}')
+    plt.xlabel(var)
+    plt.ylabel('Frecuencia')
+    plt.xlim(0, movies[var].max())
+    
+    plt.savefig(f'distribucion_{var}.png')
+    plt.close()
+
+#-----------
